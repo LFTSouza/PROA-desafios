d0acbfe8eecf69ab5d8cb1daacebfe2648d93443
Luiz Felipe Tavares <102185510+LFTSouza@users.noreply.github.com>
2025-02-07 01:39:30 -0300

    Add files via upload

---
+++ ALEABLUE/app/src/main/java/com/example/aleablue/Font/FontAlea.kt
@@ -0,0 +1,18 @@
+package com.example.aleablue.Font
+
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.aleablue.R
+
+val displayFontFamily = FontFamily(
+    Font(
+        R.font.onepiecefont,
+    )
+)
+
+val pirate = FontFamily(
+    Font(
+        R.font.pirataoneregular
+    )
+)
